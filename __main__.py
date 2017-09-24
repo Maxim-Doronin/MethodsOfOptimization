3e1300d8a7770343bda205c9d21aef2afc7765b1
PovelikinRostislav <poveliin.rostislav@gmail.com>
2017-09-24 18:38:32 +0300

    Done

--- __main__.py
+++ __main__.py
@@ -2,18 +2,19 @@
 P = 17
 # amount of years that we are living
 N = 8
-# x0 or enum of zero states
-zero_state = (0, 1, 2)
 
 
+# calculate income based on equipment age t
 def f(t):
     return 30 - t / 2
 
 
+# calculate equipments consumption based on equipment age t
 def r(t):
     return 13 + t / 2
 
 
+# calculate income of selling the equipment of age t
 def fi(t):
     if (t <= 5) and (t >= 0):
         return 7
@@ -21,20 +22,29 @@ def fi(t):
         return 3
 
 
-def s(t):
-    keep = f(t + 1) - r(t + 1) + fi(t + 2)
-    sell = fi(t + 1) + f(0) - r(0) - P + fi(1)
-    if keep < sell:
-        result = (keep, 0)
-    elif sell > keep:
-        result = (sell, 1)
+# function of total income based on current year and declared age of equipment t
+def s(t, current_year):
+    if current_year == 7:
+        keep_decision = (f(t + 1) - r(t + 1) + fi(t + 2), '0')
+        sell_decision = (fi(t + 1) + f(0) - r(0) - P + fi(1), '1')
     else:
-        result = (sell, (0, 1))
-    return result
+        keep_decision = (f(t + 1) - r(t + 1) + s(t + 1, current_year + 1)[0], '0' + s(t + 1, current_year + 1)[1])
+        sell_decision = (fi(t + 1) + f(0) - r(0) - P + s(0, current_year + 1)[0], '1' + s(0, current_year + 1)[1])
+    if keep_decision[0] >= sell_decision[0]:
+        return keep_decision
+    else:
+        return sell_decision
 
 
 def main():
+    print "last iteration"
+    for state in range(0, 3 + N):
+        print s(state, 7)
+    print "first iteration"
+    for state in range(0, 3):
+        print s(state, 0)
     return 0
+
 
 if __name__ == "__main__":
     main()
